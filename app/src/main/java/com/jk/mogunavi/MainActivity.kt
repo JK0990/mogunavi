c8155250aa7ad5cbca53bfae5aace19f8e025e20
jk <54389@naver.com>
2025-06-18 15:51:50 +0900

    Add logo image

--- app/src/main/java/com/jk/mogunavi/MainActivity.kt
+++ app/src/main/java/com/jk/mogunavi/MainActivity.kt
@@ -14,6 +14,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
 import com.jk.mogunavi.ui.theme.MoguNaviTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,14 +45,30 @@ fun MainScreen() {
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.primary),
-                contentAlignment = Alignment.Center
+        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "モグナビ",
-            color = MaterialTheme.colorScheme.onPrimary,
-            style = MaterialTheme.typography.bodyLarge.copy(
-                fontSize = 32.sp
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.mogunavi_logo),
+                contentDescription = "로고",
+                modifier = Modifier
+                    .size(256.dp) // 딱 원하는 사이즈 (Figma 기준 196이었으니 조금 더 크게)
+                    .clip(RectangleShape) // 혹시 모를 라운드 제거
+                    .border(0.dp, Color.Transparent), // 테두리 제거
+                contentScale = ContentScale.Fit // 정확한 비율 유지
             )
-        )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "モグナビ",
+                color = MaterialTheme.colorScheme.onPrimary,
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    fontSize = 32.sp
+                )
+            )
+        }
     }
 }
